de0506eaec43c3427412b452ccf57549df8af751
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2024-10-09 08:13:37 -0700

    implementing carDetailsCard in  mapsDetaispage

--- lib/presentation/pages/MapsDetailsPage.dart
+++ lib/presentation/pages/MapsDetailsPage.dart
@@ -17,19 +17,145 @@ class MapsDetailsPage extends StatelessWidget {
           onPressed: ()=>Navigator.pop(context),
         ),
       ),
-      body:FlutterMap(
-        options: MapOptions(
-          center: LatLng(51, -0.09),
-          zoom: 13,
-        ),
+      body:Stack(
         children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.rentapp.app', // Replace with your app package name
-            tileProvider: CancellableNetworkTileProvider(),// Replace with your app package name
+          FlutterMap(
+            options: MapOptions(
+              center: LatLng(51, -0.09),
+              zoom: 13,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.rentapp.app', // Replace with your app package name
+                tileProvider: CancellableNetworkTileProvider(),// Replace with your app package name
+              ),
+            ],
           ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+              child: carDetailsCard()
+          )
         ],
       )
     );
   }
+}
+
+Widget carDetailsCard(){
+  return SizedBox(
+  height: 350,
+    child: Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.black54,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+            boxShadow: [
+              BoxShadow(color: Colors.black38,spreadRadius: 0,blurRadius: 10)
+            ]
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20,),
+              Text('car.model',style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,color: Colors.white),),
+              SizedBox(height: 10,),
+              Row(
+                children: [
+                  Icon(Icons.directions_car,color: Colors.white,),
+                  SizedBox(width: 5,),
+                  Text(
+                    '>car.distance km',
+                    style: TextStyle(color: Colors.white,fontSize: 14),
+                  ),
+                  SizedBox(width: 10,),
+                  Icon(Icons.battery_full,color: Colors.white,size: 14),
+                  SizedBox(width: 5,),
+                  Text(
+                    '>car.fuelCapacity',
+                    style: TextStyle(color: Colors.white,fontSize: 14),
+                  ),
+                ],
+              )
+            ],
+          ) ,
+        ),
+       Positioned(
+         bottom: 0,
+         left: 0,
+         right: 0,
+           child: Container(
+             padding: EdgeInsets.all(20),
+             decoration: BoxDecoration(
+               color: Colors.white,
+               borderRadius: BorderRadius.only(
+                 topRight: Radius.circular(20),
+                 topLeft: Radius.circular(20)
+               )
+             ),
+             child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 Text('Features',style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
+                 featureIcons(),
+                 SizedBox(height: 20,),
+                 Row(
+                   children: [
+                     Text('\$car.pricePerHour/day',style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
+                     ElevatedButton(
+                         onPressed: (){},
+                         style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                         child: Text('Book Now',style: TextStyle(color: Colors.white),)
+                     )
+                   ],
+                 )
+               ],
+             ),
+           )
+       )
+      ],
+    ),
+  );
+}
+
+Widget featureIcons(){
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+       featureIcon(Icons.local_gas_station,'Disel','Common Rail'),
+      featureIcon(Icons.speed,'Acceleration','0 - 100km/h'),
+      featureIcon(Icons.ac_unit,'Cold','Temp Control'),
+    ],
+  );
+}
+
+Widget featureIcon(IconData icon,  String title, String subtitle) {
+return Container(
+  width: 100,
+  height: 100,
+  padding: EdgeInsets.all(5),
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(10),
+    border: Border.all(color: Colors.grey,width: 1)
+  ),
+  child: Column(
+    children: [
+      Icon(icon,size: 28,),
+      Text(title),
+      Text(
+        subtitle,
+        style: TextStyle(color: Colors.grey,fontSize: 10),
+      )
+    ],
+  ),
+);
+
 }
